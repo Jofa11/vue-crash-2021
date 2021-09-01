a17cfce4081052af5bcd5fae84cbc84def85672d
Joshua <favrejoshua@yahoo.com>
2021-08-31 22:32:38 -0500

    create router folder with index.js file and views folder with about.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@
     <AddTask @add-task="addTask" />
   </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  <Footer />
 </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask.vue'
 
@@ -18,7 +20,8 @@ export default {
   components: {
     Header,
     Tasks,
-    AddTask
+    AddTask,
+    Footer
   },
   data() {
     return {
